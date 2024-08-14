9bffaea6c8364861dbcd59890c03587f304f64ec
puranrana080 <puransinghrana080@gmail.com>
2024-08-14 22:07:16 +0530

    T9 Dynamic Routes P2 Added Cart

--- Express 1.3 00-starting-setup T8/controllers/shop.js
+++ Express 1.3 00-starting-setup T8/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart=require('../models/cart')
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -32,12 +32,23 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart=(req,res,next)=>{
+
+  const prodId=req.body.productId
+  Product.findById(prodId,(product)=>{
+Cart.addProduct(prodId,product.price)
+  })
+  
+  res.redirect('/cart')
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
