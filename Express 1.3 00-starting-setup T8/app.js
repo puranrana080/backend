b7da292aa85c7e31b21cd834880e332f53c5f82c
puranrana080 <puransinghrana080@gmail.com>
2024-08-20 22:24:41 +0530

    T20 Design the User and Product relationship

--- Express 1.3 00-starting-setup T8/app.js
+++ Express 1.3 00-starting-setup T8/app.js
@@ -14,10 +14,25 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const Product=require('./models/product')
+const User=require('./models/user');
+const { constants } = require('buffer');
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user=user
+        next()
+    })
+    .catch(err=>{console.log(err)})
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -25,10 +40,26 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-sequelize.sync()
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'})
+User.hasMany(Product)
+
+sequelize
+// .sync({force:true})
+.sync()
 .then(result=>{
     //console.log(result)
-    app.listen(3000);
+    return User.findByPk(1)
+   
+})
+.then(user=>{
+    if(!user){
+        return User.create({name:'Max',email:"test@test.com"})
+    }
+    return user
+})
+.then(user=>{
+    
+    app.listen(3000)
 })
 .catch(err=>{
     console.log(err)
